e24a8338d45f35265064103338c0818e31479fa8
Kevin <kevin@pinelabs.co>
2014-08-30 01:59:27 -0400

    v1.0 Beta
    [+] Added binary
    [+] Added sample config file

--- Globals.ps1
+++ Globals.ps1
@@ -23,14 +23,16 @@ function Test-ConfigFile
 
 function Load-ConfigFileVariables
 {
-	$btnCustom1.Text = $btnCustom1Text
-	$btnCustom2.Text = $btnCustom2Text
-	$btnCustom3.Text = $btnCustom3Text
-	$btnCustom4.Text = $btnCustom4Text
-	$btnCustom4.Text = $btnCustom4Text
-	$btnCustom5.Text = $btnCustom5Text
-	$btnCustom6.Text = $btnCustom6Text
-	$btnCustom7.Text = $btnCustom7Text
+	$btnCustom1.Text = $CustomButton1_Text
+	$btnCustom2.Text = $CustomButton2_Text
+	$btnCustom3.Text = $CustomButton3_Text
+	$btnCustom4.Text = $CustomButton4_Text
+	$btnCustom4.Text = $CustomButton4_Text
+	$btnCustom5.Text = $CustomButton5_Text
+	$btnCustom6.Text = $CustomButton6_Text
+	$btnCustom7.Text = $CustomButton7_Text
+	$btnCustom8.Text = $CustomButton8_Text
+	$btnCustom9.Text = $CustomButton9_Text
 }
 
 function Load-Buttons
@@ -42,6 +44,8 @@ function Load-Buttons
 	if ($btnCustom5.Text -eq '') { $btnCustom5.Text = "Custom Button 5"; $btnCustom5.Enabled = $false }
 	if ($btnCustom6.Text -eq '') { $btnCustom6.Text = "Custom Button 6"; $btnCustom6.Enabled = $false }
 	if ($btnCustom7.Text -eq '') { $btnCustom7.Text = "Custom Button 7"; $btnCustom7.Enabled = $false }
+	if ($btnCustom8.Text -eq '') { $btnCustom8.Text = "Custom Button 8"; $btnCustom8.Enabled = $false }
+	if ($btnCustom9.Text -eq '') { $btnCustom9.Text = "Custom Button 9"; $btnCustom9.Enabled = $false }
 }
 
 Function logstamp
@@ -86,17 +90,21 @@ function Disable-Buttons
 	$btnCustom2.Enabled = $false
 	$btnCustom6.Enabled = $false
 	$btnCustom7.Enabled = $false
+	$btnCustom8.Enabled = $false
+	$btnCustom9.Enabled = $false
 }
 
 function Enable-Buttons
 {
 	$btnCustom1.Enabled = $true
-	$btnCustom5.Enabled = $true
+	$btnCustom2.Enabled = $true
 	$btnCustom3.Enabled = $true
 	$btnCustom4.Enabled = $true
-	$btnCustom2.Enabled = $true
+	$btnCustom5.Enabled = $true
 	$btnCustom6.Enabled = $true
 	$btnCustom7.Enabled = $true
+	$btnCustom8.Enabled = $false
+	$btnCustom9.Enabled = $false
 }
 
 function Clear-Formats
---
+++ Sample Config File/PowerLauncher_Config.ps1
@@ -0,0 +1,31 @@
+
+
+# PowerLauncher Custom Configuration
+# Disclaimer: tampering with this file can cause the application to malfunction or not work, at all
+
+$CustomButton1_Text = "Notepad"
+$CustomButton1_Script = "notepad.exe"
+
+$CustomButton2_Text = "Word"
+$CustomButton2_Script = "winword.exe"
+
+$CustomButton3_Text = "PowerShell"
+$CustomButton3_Script = "powershell.exe"
+
+$CustomButton4_Text = "Excel"
+$CustomButton4_Script = "excel.exe"
+
+$CustomButton5_Text = "Cmder"
+$CustomButton5_Script = "C:\bin\cmder\cmder.exe"
+
+$CustomButton6_Text = "Google"
+$CustomButton6_Script = "https://www.google.com"
+
+$CustomButton7_Text = ""
+$CustomButton7_Script = ""
+
+$CustomButton8_Text = "vN3rd GitHub"
+$CustomButton8_Script = "https://github.com/vN3rd"
+
+$CustomButton9_Text = ""
+$CustomButton9_Script = ""
