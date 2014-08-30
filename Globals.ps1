01af244550fb2a972747ca454f54fdee9117b372
Kevin <kevin@pinelabs.co>
2014-08-29 23:50:36 -0400

    Added back

---
+++ Globals.ps1
@@ -0,0 +1,22 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{ 
+	if($hostinvocation -ne $null)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+
